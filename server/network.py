d19b11eaf0d9287750102c121458574993696b47
Daniel Pfurner <dpfurner@tsn.at>
2023-02-07 19:53:17 +0100

    Refactor Information of the Server Network

--- server/network.py
+++ server/network.py
@@ -4,5 +4,5 @@ In this File, the Network connection is defined.
 It includes a class to allow clients to connect to the server,
 store the clients as object and handle the communication between the server and the clients.
 
-IMPORTANT: This is the only file that is used to access the network. (Only file with socket objects
+IMPORTANT: This is the only file that is used to access the network. (Only file with socket objects)
 """
