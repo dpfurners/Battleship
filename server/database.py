111764f3ad62bfaa13dbb775cd6a1ecc43f915cb
Daniel Pfurner <dpfurner@tsn.at>
2023-02-07 19:36:21 +0100

    Provide Structure/Information about the Server

---
+++ server/database.py
@@ -0,0 +1,7 @@
+"""
+In this File, the Database Interface is defined.
+
+It includes a class to connect to the database and functions to handle data exchange between the database and the server.
+
+IMPORTANT: This is the only file that is used used to access the database. (Only file with sql queries)
+"""
---
+++ server/logic.py
@@ -0,0 +1,5 @@
+"""
+This File contains the logic of the server.
+
+It connects the server network, the database interface and provides to logic to play the game.
+"""
