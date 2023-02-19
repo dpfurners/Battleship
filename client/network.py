3504475e6c08bfb08bb87357895cfa391ad1da44
aleksxmrkvc <aleks3@gmx.at>
2023-02-19 16:53:48 +0100

    Basic client setup

--- client/network.py
+++ client/network.py
@@ -5,3 +5,23 @@ It includes a class to connect to the server and handle the communication to the
 
 IMPORTANT: This is the only file that is used to access the network. (Only file with socket objects)
 """
+import socket 
+
+class NetworkClientBase:
+    def __init__(self, host: str | None = None, port: int | None = None) -> None:
+
+        self.addr = (host, port)
+        self.conn: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+
+        self.conn.connect(self.addr)
+
+    def recv(self, size: int) -> bytes:
+
+        return self.conn.recv(size)
+
+    def send(self, data: bytes):
+
+        self.conn.send(data)
+
