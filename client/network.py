dd347c886e75ab779d901e40dbdd7abf5b7a4c70
aleksxmrkvc <aleks3@gmx.at>
2023-02-26 22:19:50 +0100

    Client Code überarbeitet (Lobby)

--- client/network.py
+++ client/network.py
@@ -9,30 +9,35 @@ import socket
 
 class NetworkClientBase:
     def __init__(self, host: str | None = None, port: int | None = None) -> None:
-
+    # Host (Name oder IP des Servers) und Port (Nummer, auf dem der Server lauscht)
+    # werden als Argumente akzeptiert
         self.addr = (host, port)
         self.conn: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Instanzvariablen werden initialisiert
 
-    def connect(self, username: str, password: str) -> bool:
-
+    def connect(self, username: str, password: str, lobby: str) -> bool:
+    # Zeigt, ob verbunden wurde oder nicht
         self.conn.connect(self.addr)
-        data = f"{username}-{password}"
+        # Verbindung mit Server mit den Adressdaten von "self.addr" aufgebaut
+        data = f"{username}-{password}-{lobby}"
         self.send(data.encode())
+        # Nachricht wird erstellt und an Server geschickt + Umwandlung in Bytes
         approved = self.recv(1024).decode()
+        # Antwort vom Server wird erwartet + Umwandlung in einen String
         if approved == "connected":
             return True
         else:
             return False
+        # Es wird geschaut, ob der Server die Verbindung akzeptiert hat oder nicht
 
     def recv(self, size: int) -> bytes:
-
+    # Nachricht mit bestimmter Größe wird empfangen und zurückgegeben
         return self.conn.recv(size)
 
     def send(self, data: bytes):
-
+    # Byte Objekt wird an Server geschickt
         self.conn.send(data)
 
-if __name__ == '__main__':
-    client = NetworkClientBase("localhost", 1234)
-    print(client.connect("aleks", "Baum1234"))
+
+
 
