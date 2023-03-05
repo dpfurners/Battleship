edf8428575b6660ff109dc1a8ab7d2768fa126e6
emanuel12234456 <123064476+emanuel12234456@users.noreply.github.com>
2023-03-06 00:58:12 +0100

    Add files via upload

---
+++ client/gui/matches.py
@@ -0,0 +1,66 @@
+from functools import partial
+
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QScrollArea, QPushButton, QLabel, QBoxLayout, QMessageBox
+
+
+class MatchingPage(QScrollArea):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.available = []
+
+        self.widgt = QWidget()
+        self.layout = QVBoxLayout(self.widgt)
+        self.setLayout(self.layout)
+
+        self.setWidget(self.widgt)
+
+    def init(self):
+        self.parent().match_timer.timeout.connect(self.matching_update)
+        self.parent().match_timer.start(1000)
+        self.matching_update()
+        self.parent().setFixedSize(400, 400)
+        self.setWidgetResizable(False)
+        self.parent().setWindowTitle("Battleship - Matching")
+
+        self.update()
+
+    def update(self):
+        self.widgt = QWidget()
+        self.layout = QVBoxLayout(self.widgt)
+
+        if self.available:
+            for client in self.available:
+                btn = QPushButton(self)
+                btn.setFixedSize(360, 35)
+                btn_layout = QBoxLayout(QBoxLayout.Direction.LeftToRight)
+                btn_label = QLabel(client[0], self)
+                btn_label.setStyleSheet("text-align: right; padding-right: 5px")
+                btn_layout.addWidget(btn_label)
+                btn_layout.addWidget(QLabel(f"Games won: {client[1]}", self))
+                style_sheet = f"background-color: {'green' if client[2] else 'red'}; " \
+                              f"color: {'green' if client[3] else 'black'};"
+                btn.setLayout(btn_layout)
+                btn.setStyleSheet(style_sheet)
+                btn.clicked.connect(partial(self.parent().connection.send, f"match\n{client[0]}"))
+                self.layout.addWidget(btn)
+            self.setWidget(self.widgt)
+
+        else:
+            lbl = QLabel("No matches available")
+            lbl.setStyleSheet("margin: 10px;")
+            self.setWidget(lbl)
+
+    def matching_update(self) -> None:
+        try:
+            self.parent().connection.send("get_available")
+        except ConnectionResetError:
+            self.parent().set_page("login")
+            self.parent().connection.close()
+            return
+        new = self.parent().connection.recv()
+        if new == "game":
+            self.parent().match_timer.stop()
+            self.parent().set_page("game")
+            return
+        self.available = new
+        self.update()
