6cdf62164bb10bf564ad953e4d48fa738e83f112
aleksxmrkvc <aleks3@gmx.at>
2023-03-05 16:52:52 +0100

    Alte sachen überarbeitet, Schiff model hinzugefügt

--- client/network.py
+++ client/network.py
@@ -5,35 +5,43 @@ It includes a class to connect to the server and handle the communication to the
 
 IMPORTANT: This is the only file that is used to access the network. (Only file with socket objects)
 """
-import socket 
+import socket
+import pickle
+
 
 class NetworkClientBase:
-    def __init__(self, host: str | None = None, port: int | None = None) -> None:
+    def __init__(self, host: str | None = "localhost", port: int | None = 1234) -> None:
 
         self.addr = (host, port)
         self.conn: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Instanzvariablen werden initialisiert
 
-    def connect(self, username: str, password: str, lobby: str) -> bool:
+    def reset(self):
+        self.conn.close()
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Verbindung wird geschlossen und neu erstellt
+
+    def connect(self, username: str, password: str) -> bool | str:
         self.conn.connect(self.addr)
         # Verbindung zur Adresse wird hergestellt
-        data = f"{username}-{password}-{lobby}"
+        data = f"{username}-{password}"
         # Datenstrom
-        self.send(data.encode())
+        self.send(data)
         # An Server gesendet
-        approved = self.recv(1024).decode()
+        approved = self.recv()
         # Empfangen vom Server
         if approved == "connected":
             return True
         else:
-            return False
+            return approved
         # Schaut, ob Verbindung besteht
-    def recv(self, size: int) -> bytes:
-    # Erhalten von Daten als Bytes
-        return self.conn.recv(size)
 
-    def send(self, data: bytes):
-    # Sendet von Daten als Bytes
-        self.conn.send(data)
+    def recv(self) -> any:
+        # Erhalten von Daten als Bytes
+        return pickle.loads(self.conn.recv(1024*2))
+
+    def send(self, data: any):
+        # Sendet von Daten als Bytes
+        self.conn.send(pickle.dumps(data))
 
 
--- server/logic.py
+++ server/logic.py
@@ -3,3 +3,27 @@ This File contains the logic of the server.
 
 It connects the server network, the database interface and provides to logic to play the game.
 """
+import threading
+
+from server.network.interface import NetworkServerWithLobby
+from server.database import DBInterface
+
+
+class ServerLogic:
+    def __init__(self, host: str = "localhost", port: int = 1234):
+        self.db = DBInterface()
+        self.network = NetworkServerWithLobby(host, port, self.db)
+
+        self.accept_thread = threading.Thread(target=self.network.accept_new)
+
+    def run(self):
+        self.accept_thread.start()
+        try:
+            while True:
+                pass
+        except KeyboardInterrupt:
+            self.close()
+
+    def close(self):
+        self.network.close()
+        self.db.Close()
